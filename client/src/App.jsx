0943c0a215c5c441711a768b3564857a99c90a2c
lbrMar <mar.lm@protonmail.com>
2024-01-13 18:23:11 -0800

    Add styles and bg image for landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,10 @@ import AsciiArt from './components/AsciiArt/AsciiArt'
 function App() {
 
   return (
-    <>
+    <div className={styles.mainContainer}>
       <button className={styles.btn}>click me</button>
       <AsciiArt />
-    </>
+    </div>
   )
 }
 
