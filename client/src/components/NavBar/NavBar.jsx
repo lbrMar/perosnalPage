34c3305b1b18676e044cffd6a97da12db626ae79
lbrMar <mar.lm@protonmail.com>
2024-01-14 16:51:07 -0800

    add fixed navbar top

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,20 @@
+import styles from './NavBar.module.css'
 
 function NavBar() {
 
   return (
-    <div></div>
+    <div>
+      <div className={styles.flexRowContainer}>
+        <p>{`@lbrm`}</p>
+        <div className={styles.linksContainer}>
+          <p>{`// home`}</p>
+          <p>{`// about`}</p>
+          <p>{`// arcade`}</p>
+          <p>{`// contact`}</p>
+          <p>{`// git`}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
