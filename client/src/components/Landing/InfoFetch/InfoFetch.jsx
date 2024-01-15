04c145659c056323f2d7d9df455429a813ca408d
lbrMar <mar.lm@protonmail.com>
2024-01-14 16:03:09 -0800

    Add display content for landing page

---
+++ client/src/components/Landing/InfoFetch/InfoFetch.jsx
@@ -0,0 +1,26 @@
+import styles from './InfoFetch.module.css'
+
+function InfoFetch() {
+  return (
+    <div className={styles.flexColumnContainer}>
+      <div className={styles.textContainer}>
+        <p className={styles.pTitle}>Lukas@Myhal</p>
+      </div>
+      <p>-----------------------</p>
+      <div className={styles.textContainer}>
+        <p className={styles.pTitle}>Skills:</p>
+        <p className={styles.pInfo}>Javascript React CSS HTML NODE</p>
+      </div>
+      <div className={styles.textContainer}>
+        <p className={styles.pTitle}>Git Hub:</p>
+        <p className={styles.pInfo}>https://github.com/lbrMar</p>
+      </div>
+      <div className={styles.textContainer}>
+        <p className={styles.pTitle}>University:</p>
+        <p className={styles.pInfo}>Wilfrid Laurier University FRENCH MAJOR</p>
+      </div>
+    </div>
+  )
+}
+
+export default InfoFetch
--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,9 +1,17 @@
 import styles from './Landing.module.css'
+/***************************** import components *****************************/
+import AsciiArt from './AsciiArt/AsciiArt'
+import InfoFetch from './InfoFetch/InfoFetch'
 
 function Landing() {
 
   return (
-    <div className={styles.mainContainer}></div>
+    <div className={styles.pageContainer}>
+      <div className={styles.displayContainer}>
+        <AsciiArt />
+        <InfoFetch />
+      </div>
+    </div>
   )
 }
 
