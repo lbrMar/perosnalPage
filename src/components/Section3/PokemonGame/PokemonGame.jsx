21651f6367cae310c3c6cd7ec5beb96047d3a942
lbrMar <mar.lm@protonmail.com>
2024-01-29 13:43:03 -0800

    add icon to pokemon game

--- src/components/Section3/PokemonGame/PokemonGame.jsx
+++ src/components/Section3/PokemonGame/PokemonGame.jsx
@@ -1,8 +1,10 @@
 import './PokemonGame.css'
+import chevronsLeft from '../../../assets/icons/featherIcons/chevrons-left.svg'
 
 const PokemonGame = () => {
   return (
     <div className='pokemonGameMainContainer'>
+      <img src={chevronsLeft} alt='Left Arrow' />
       <p>Test</p>
     </div>
   )
